73e13fb978a15f9980a46013a8cf9ceb4095d30b
luckygxf <403261934@qq.com>
2017-04-09 23:15:01 +0800

    add introduc

--- index.php
+++ index.php
@@ -3,19 +3,20 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Simple</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/font-awesome.css">
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/templatemo-style.css">
-        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
-    </head>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Simple</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/templatemo-style.css">
+	<link rel="stylesheet" href="css/introduction.css">
+	<script src="js/vendor/modernizr-2.6.2.min.js"></script>
+</head>
     <body>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
@@ -82,8 +83,8 @@
         <div class="banner-bg" id="top">
             <div class="banner-overlay"></div>
             <div class="welcome-text">
-                <h2>Simple Design | Personal Portfolio</h2>
-                <h5>This is a mobile friendly layout with Bootstrap v3.3.1 framework. Maecenas eu ante at nunc posuere fringilla sit amet non dolor. Proin condimentum fermentum nunc.</h5>
+                <h2>Simple Life | Personal Space</h2>
+                <h5>To be simple & Live Simple.</h5>
             </div>
         </div>
 
@@ -101,7 +102,46 @@
                             <div class="about-image">
                                 <img src="image/8.jpg" alt="about me">
                             </div>
-                            <p>Volton is free <a rel="nofollow" href="http://www.templatemo.com/page/1">responsive mobile template</a> from <span class="blue">template</span><span class="green">mo</span> website. You can use this template for any purpose. Please tell your friends about it. Thank you. Credit goes to <a rel="nofollow" href="#">Unsplash</a> for images used in this design. You can <strong>change menu icons</strong> by checking <a rel="nofollow" href="#/font-awesome-icon-world-map/">Font Awesome</a> (version 4). Example: <strong>&lt;i class=&quot;fa fa-camera&quot;&gt;&lt;/i&gt;</strong></p>
+							<div class="list">
+								<h2>后台开发攻城狮 | 58赶集 | 2016.07 - </h2>
+								<h3><u>技能</u></h3>
+								<ul>
+									<li>Java Web开发：Servlet开发 | Tomcat使用 | 编写JSP</li>
+									<li>Java并发编程：多线程的编写 | concurrent包的使用 | Java线程池</li>
+									<li>S2SH框架：熟悉MVC | 三层开发 | 了解Spring IOC、AOP</li>
+									<li>TCP/IP、HTTP协议：TCP/IP协议栈 | TCP、UDP、IP协议 | HTTP协议</li>
+									<li>数据结构&算法：链表 | 栈 | 队列 | 树 | 动态规划 | 回溯 | BFS | DFS</li>
+									<li>Redis、Mybatis：熟悉redis | mybatis的使用</li>
+									<li>Android开发：控件使用 | 控件布局 | 多线程 | 自定义控件</li>
+								</ul>
+								<h3><u>项目经历</u></h3>
+									<ul>
+										<li>
+										2016.09-2016.10 | 校验登录生成token | 1人
+										项目描述：通过检验用户是否登录passport，生成im服务的token
+										个人职责：设计和实现系统
+										</li>
+										<li>
+										2016.07-2016.12 | wredis开发与维护 | 6人
+										项目描述：redis云存储，用户通过申请使用redis，管理员分配redis给用户使用。项目通过web管理平台可视化管理redis实例，agent接收命令和采集数据，客户端有上报命令执行情况功能
+										个人职责：web管理平台的开发与维护，java客户端测试
+										</li>
+										<li>
+										2014.09-2014.10 | 基于Android的智能家居灯控App开发 | 3人
+										项目描述：APP通过自定义协议，打包命令,通过Socket使用TCP和UDP协议发送数据包到灯控。灯控解析数据包，实现对灯的控制
+										个人职责：根据需求文档，设计并实现部分界面
+										</li>
+										<li>
+										2014.06-2014.09 | 基于Android的比赛记分牌App的开发 | 1人
+										项目描述：APP通过自定义协议，将要发送的命令打包。手机Wifi连接到记分牌的Wifi，实现通信。通过Socket使用UDP协议发送数据包到记分牌
+										个人职责：设计并实现App
+										</li>
+										<li>
+										2011.06-2011.10 | 绩效考核系统设计与开发 | 8人	项目描述：项目使用SSH框架加三层架构，使用MVC模型进行开发。Struts2负责将页面请求转发到后台，Spring将Java实例通过DI注入到程序中，Hibernate负责将数据读取和保存。DAO层负责数据库操作，Service层负责逻辑处理，JSP负责显示
+										个人职责：框架的搭建、登录模块、功能管理、员工信息、岗位信息等基础信息模块
+										</li>
+									</ul>
+							</div>
                             <hr>
                         </div>
                     </div> <!-- #about -->
